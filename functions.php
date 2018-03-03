aa6f09597c323a835d8c6adbbaac16aae6df043b
Ezekiel <rajezekielangel@gmail.com>
2018-03-03 15:05:41 +0530

    logo added

--- functions.php
+++ functions.php
@@ -8,3 +8,17 @@ function school_new_the_custom_logo() {
 }
 endif;
 
+if ( ! function_exists( 'school_new_setup' ) ) :
+
+function school_new_setup() {
+
+add_theme_support( 'custom-logo', array(
+                'height'      => 248,
+                'width'       => 248,
+                'flex-height' => true,
+        ) );
+
+}
+endif;
+add_action( 'after_setup_theme', 'school_new_setup' );
+
--- header.php
+++ header.php
@@ -21,6 +21,22 @@
 
 <div id="header">
 
+<?php 
 
+school_new_the_custom_logo(); 
+
+
+if ( is_front_page() && is_home() ) : ?>
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+<?php else : ?>
+      <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+<?php endif;
+
+$description = get_bloginfo( 'description', 'display' );
+if ( $description || is_customize_preview() ) : ?>
+<p class="site-description"><?php echo $description; ?></p>
+<?php endif;
+
+?>
 </div>
 
