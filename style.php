46039a2620616c3a70f13a3958e1b3c0e8dc4f81
Ezekiel <rajezekielangel@gmail.com>
2018-03-03 13:54:03 +0530

    initial commit

---
+++ style.php
@@ -0,0 +1,11 @@
+/*
+Theme Name: School TEmplate 
+Theme URI: https://github.com/ezekielraj/school_new
+Author: Ezekiel
+Description: theme created for st.antonys school.
+Version: 1.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: bootstrap, st.antonys
+Text Domain: school_new
+*/
