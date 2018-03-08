08ad3c95cba6a5f57ed0dc7df46b5c81e270d613
Ezekiel Angel Raj <ezekiel.r@hcl.com>
2018-03-08 14:54:15 +0530

    check

--- page.php
+++ page.php
@@ -8,6 +8,22 @@ page for single page
 
 
 
+                <?php
+                // Start the loop.
+                while ( have_posts() ) : the_post();
+
+                        // Include the page content template.
+                        get_template_part( 'content', 'page' );
+
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                                comments_template();
+                        endif;
+
+                // End the loop.
+                endwhile;
+                ?>
+
 
 
 
--- single.php
+++ single.php
@@ -7,6 +7,20 @@ get_header(); ?>
 for single post content
 
 
+                             <?php while ( have_posts() ) : the_post(); ?>
+                                        <?php get_template_part( 'template-parts/content',get_post_format() ); ?>
+   					<?php
+                                        // If comments are open or we have at least one comment, load up the comment template.
+                                        if ( comments_open() || get_comments_number() ) :
+                                                comments_template();
+                                        endif;
+
+                                        if ( ! comments_open() ) {
+                                                _e( 'Comments are closed.', 'school' );
+                                        }
+                                        ?>
+                                </div>
+                                <?php endwhile; // End of the loop. ?>
 
 
 
