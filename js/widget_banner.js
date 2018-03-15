fa0fa2e7850a31902b89804c7c7a71e13cf0ce80
Ezekiel <rajezekielangel@gmail.com>
2018-03-15 23:52:28 +0530

    widget style

---
+++ js/widget_banner.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("#widget_banner > div:gt(0)").hide();
+    
+    setInterval(function() { 
+      $('#widget_banner > div:first')
+        .css("display","none")//fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#widget_banner');
+    },  3000);
+
+   
+    
+    //use setinterval for slider loop
+});
